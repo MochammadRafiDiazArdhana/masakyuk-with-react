f224217490b8431a5a75041818346d9aba37a3b6
MochammadRafiDiazArdhana <mochammad.20098@mhs.unesa.ac.id>
2023-06-30 06:36:55 +0700

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./Routes/Home";
+import Ingredients from "./Routes/Ingredients";
+import RecipeForm from "./Routes/RecipeForm";
+import Profile from "./Routes/Profile";
+import Login from "./Routes/Login";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+        <NavBar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/ingredients" element={<Ingredients />} />
+          <Route path="/recipeForm" element={<RecipeForm />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
